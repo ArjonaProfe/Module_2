a48fab321dc08cc1d63df6f248e6b38d2fb92731
Kanarath <117662948+Kanarath@users.noreply.github.com>
2022-11-25 16:51:35 +0100

    Double Jump Review

--- Assets/Gerard/SCRIPTS NTRM/INPUT/RollGerard.cs
+++ Assets/Gerard/SCRIPTS NTRM/INPUT/RollGerard.cs
@@ -8,7 +8,6 @@ public class RollGerard : MonoBehaviour
     public bool flipped;                        // hago publico el boleano flipped para hacer el roll en la dirección correcta.
     public float xRoll;                        // hago publico el parametro xRoll para añadirle velocidad al Roll.
     private Rigidbody2D rb;                   // hago privado la asiganción de RigidBody2D a rb.
-    private float xMovement;                 // hago privado el parametro xMovement para coger los datos luego.
     public bool rollCD;                     // hago publico el boleano rollCD para poder ponerle un CD.
 
     void Start()
@@ -20,7 +19,10 @@ public class RollGerard : MonoBehaviour
 
     void Update()
     {
-        xMovement = Input.GetAxisRaw("Fire3");                                 // se asigna el valor del "Fire3" a la variable xMovement.
-        rb.velocity = new Vector2(xMovement * xRoll, rb.velocity.y);           // al valor "velocity" del rigidbody 2d "rb" se le asigna el valor en X : xMovement multiplicado por xRoll y se le deja en Y el valor que tenga.
+        if (Input.GetButton("Fire3"))
+        {
+            rb.AddForce(transform.right * xRoll);
+            Debug.Log("Fire3");
+        }
     }
 }
